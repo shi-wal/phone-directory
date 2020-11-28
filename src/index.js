3124363a1f580d54c2cc09aaf00922cc2c395c69
Shivangi Agrawal <shivangiagrawal667@gmail.com>
2020-11-28 22:36:15 +0530

    Added 'AddSubscriber' component

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AddSusbscriber from './AddSubscriber';
+import './common/common.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AddSusbscriber />
   </React.StrictMode>,
   document.getElementById('root')
 );
